0586d4c49a28bfcec5add780a2b1218241dbc745
Hassan Sherwani <hassansherwani7878@gmail.com>
2026-06-11 17:57:12 +0500

    expense.py in DsService

--- backend/dsService/src/app/schema/expense.py
+++ backend/dsService/src/app/schema/expense.py
@@ -1,8 +1,24 @@
 from typing import Optional
 from pydantic import BaseModel, Field
 
+
 class Expense(BaseModel):
     """Information about a transaction made on any Card"""
-    amount: Optional[str] = Field(title="expense", description="Expense made on the transaction")
-    merchant: Optional[str] = Field(title="merchant", description="Marchant name whom the transaction has been made")
-    currency: Optional[str] = Field(title="currency", description="currency of the transaction")
+
+    amount: Optional[str] = Field(
+        default=None,
+        title="expense",
+        description="Expense made on the transaction"
+    )
+
+    merchant: Optional[str] = Field(
+        default=None,
+        title="merchant",
+        description="Merchant name whom the transaction has been made"
+    )
+
+    currency: Optional[str] = Field(
+        default=None,
+        title="currency",
+        description="Currency of the transaction"
+    )
